178aae24dd741f2792eece49c55026776c65c26a
chitradip97 <chitradip97@gmail.com>
2023-12-28 22:52:36 +0530

    Qst_19 solved

---
+++ callback function/Qst_19.js
@@ -0,0 +1,9 @@
+// Write a function that takes an array of numbers and returns the maximum value. Use the forEach method.
+
+const numbers = [1, 2, 53, 40, 85, 66, 27, 18, 69, 10];
+let max_num=0;
+let max=num=>{
+ max_num<num?max_num=num:''
+}
+numbers.forEach(max);
+console.log(max_num);

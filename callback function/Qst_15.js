777463557e258c10da37cb476454b38128c87515
chitradip97 <chitradip97@gmail.com>
2023-12-28 21:09:36 +0530

    Qst_15 done

---
+++ callback function/Qst_15.js
@@ -0,0 +1,11 @@
+// Write a function that takes an array of numbers and returns the sum of the squares of each number. Use the forEach method.
+
+
+const numbers = [1, 2, 3, 4, 5];
+let sum=0
+let exp=num=>{
+    sum+=num*num;
+    // console.log(sum);
+}
+numbers.forEach(exp);
+console.log(sum);

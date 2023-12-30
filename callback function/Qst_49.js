b3a3920ff3c26329eee08d4ac26d1952044dcb6a
chitradip97 <chitradip97@gmail.com>
2023-12-30 13:24:51 +0530

    Qst 49 solve

---
+++ callback function/Qst_49.js
@@ -0,0 +1,4 @@
+// Use the some function to check if there is any negative number in an array.
+const numbers = [10, 30, -50, 170, 90];
+verify=numbers.some(number=>number<0);
+console.log("Output :"+verify);

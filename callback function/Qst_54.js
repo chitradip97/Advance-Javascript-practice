540d32259734d044aa1d3acc9347e0bb81383dc5
chitradip97 <chitradip97@gmail.com>
2023-12-30 13:59:30 +0530

    qst 54 solved

---
+++ callback function/Qst_54.js
@@ -0,0 +1,4 @@
+// Given an array of numbers, use the find function to locate the first element greater than 10.
+const numbers = [5, 8, 12, 3, 15];
+let findvalue=numbers.find(number=>number>10)
+console.log("value :"+findvalue);

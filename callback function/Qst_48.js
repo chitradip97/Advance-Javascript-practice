7d16d5caf8afbdf1e888503ae69aaad3b7e20ac1
chitradip97 <chitradip97@gmail.com>
2023-12-30 13:19:44 +0530

    qst 48 solve

---
+++ callback function/Qst_48.js
@@ -0,0 +1,5 @@
+// Use the some function to find if an array contains an element equal to a specific value.
+
+const numbers = [10, 30, 50, 170, 90];
+verify=numbers.some(number=>number==50);
+console.log("Output :"+verify);

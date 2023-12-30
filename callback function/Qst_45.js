ce23f81fd865a45c9bc7a2fb78e6d94d19308e8e
chitradip97 <chitradip97@gmail.com>
2023-12-30 10:43:36 +0530

    qst 45 solve

---
+++ callback function/Qst_45.js
@@ -0,0 +1,6 @@
+// Custom Condition:
+
+// Write a function that uses the every function to check if all elements in an array are even.
+const numbers = [10, 30, 50, 170, 90];
+let checknum=numbers.every(num=>num%2==0);
+console.log("output :"+checknum);

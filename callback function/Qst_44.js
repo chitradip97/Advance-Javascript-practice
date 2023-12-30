24184e8c289babe45e29d043bf457f3cf8ba06e1
chitradip97 <chitradip97@gmail.com>
2023-12-30 10:39:09 +0530

    qst 44 solved

---
+++ callback function/Qst_44.js
@@ -0,0 +1,6 @@
+// Use the some function to check if any element in an array is greater than 10.
+
+const numbers = [1, 3, 5, 17, 9];
+
+let checknum=numbers.some(num=>num>10);
+console.log("output :"+checknum);
